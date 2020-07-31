26df0ed95d0c1292290f9e5bb91c791528406f38
weirdwombat <omkennedy@dmacc.edu>
2020-07-31 15:15:23 -0500

    What I have so far

---
+++ proof_monty_hall.py
@@ -0,0 +1,15 @@
+switch_wins = []
+user_input = random(1,4)
+prize_door = random(1,4)
+
+#need a for loop with a range
+def door_selection():
+    if user_input != (1,2,3):
+        assert ValueError
+    else:
+
+def switch_or_stay():
+    if user_input != prize_door:
+        switch_wins += 1
+
+#switch_wins_percentage = (switch_wins[0]/number of times I run the loop)
