0566f5e8273a15e409303b7204fc3b1e24ac21ae
weirdwombat <omkennedy@dmacc.edu>
2020-08-01 16:37:59 -0500

    A little bit better

--- proof_monty_hall.py
+++ proof_monty_hall.py
@@ -1,22 +1,41 @@
 import matplotlib.pyplot as plt
 
 switch_wins = []
-user_input = random(1,4)
-prize_door = random(1,4)
+user_input = random.randrange(1,4)
+prize_door = random.randrange(1,4)
 
-# will eventually organize into a class
 #need a for loop with a range
-def door_selection():
-    if user_input != (1,2,3):
-        assert ValueError
-    else:
+class InvalidDoorSelection(Exception):
+    passs
 
-def switch_or_stay():
-    if user_input != prize_door:
-        switch_wins += 1
+def door_selection():
+    if user_input not in [1,2,3]:
+        raise  InvalidDoorSelection
+    else:
+        final()
+
+#i want the program to be able to be able to reveal a remaining door that does not have the prize
+#behind it
+
+def final():
+    if(user_pick != 1 and prize_door != 1):
+        doors[0] = 'host revealed'
+    elif(user_pick != 2 and prize_door != 2):
+        doors[1] = 'host revealed'
+    elif(user_pick != 3 and prize_door != 3):
+        doors[1] = 'host revealed'
+
+    def switch_or_stay():
+        if user_input != prize_door:
+            switch_wins += 1
+    switch_or_stay()
+    #gui()
+
 
 #switch_wins_percentage = (switch_wins[0]/number of times I run the loop)
 
 #plt.plot(decimal on x axis, ?? on y-axis)
 
 #plt.xlabel("Wins by switching")
+#plt.ylabel(not sure what y axis will be yet)
+
